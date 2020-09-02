dbe7c89cc2be4ef3019251d34699d4d15b95d75f
Zackary Cammack <zack.cammack@gmail.com>
2020-09-02 20:42:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog
 
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
 
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
 
 end
